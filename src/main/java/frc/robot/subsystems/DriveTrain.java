1bd25a5e3c4586149f9912b9b7b0235d6db73785
ZYXWong24680 <wongz1@bxscience.edu>
2019-02-18 15:51:55 -0500

    joystick lift with upper and lower bounds

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -27,18 +27,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
 
   public static CANSparkMax frontRight = new CANSparkMax(14, MotorType.kBrushless);
-  public static CANSparkMax frontLeft = new CANSparkMax(15, MotorType.kBrushless);
-  //public static CANSparkMax rearRight = new CANSparkMax(15, MotorType.kBrushless);
+  //public static CANSparkMax frontLeft = new CANSparkMax(15, MotorType.kBrushless);
+  public static CANSparkMax rearRight = new CANSparkMax(15, MotorType.kBrushless);
   //public static CANSparkMax rearLeft = new CANSparkMax(12, MotorType.kBrushless);
   //public static CANSparkMax middleLeft = new CANSparkMax(10, MotorType.kBrushless);
   //public static CANSparkMax middleRight = new CANSparkMax(18, MotorType.kBrushless);
 
 //
   public static double initial, initial2;
+  public static double max = -40;
   //public static AnalogGyro gyro = new AnalogGyro(RobotMap.gyroPort);
 
-  //public static CANEncoder frontRightHall = frontRight.getEncoder();
-  //public static CANEncoder rearRightHall = rearRight.getEncoder();
+  public static CANEncoder frontRightHall = frontRight.getEncoder();
+  public static CANEncoder rearRightHall = rearRight.getEncoder();
   /*public static CANEncoder frontLeftHall = frontLeft.getEncoder();
   public static CANEncoder rearLeftHall = rearLeft.getEncoder();
   public static CANEncoder middleLeftHall = middleLeft.getEncoder();
@@ -48,10 +49,10 @@ public class DriveTrain extends Subsystem {
   //public static TalonSRX frontRight = new TalonSRX(1);
 
   public DriveTrain(){
-    //frontRightHall.setPosition(0.0);
-    //rearRightHall.setPosition(0.0);
-    //initial = frontRightHall.getPosition();
-    //initial2 = rearRightHall.getPosition();
+    frontRightHall.setPosition(0.0);
+    rearRightHall.setPosition(0.0);
+    initial = frontRightHall.getPosition();
+    initial2 = rearRightHall.getPosition();
   }
 
   @Override
@@ -60,34 +61,45 @@ public class DriveTrain extends Subsystem {
     System.out.println("defaultcommand");
   }
 
-  /*
+  
   public static void driveTeleop(){//cascade
-    //double leftJoy = OI.atkJoy1.getRawAxis(1);
-    double rightJoy = -OI.atkJoy1.getRawAxis(5);
+    //everything is mechanically backwards
 
-    //frontRight.set(ControlMode.PercentOutput, rightJoy);
-    if (frontRightHall.getPosition() < initial && rearRightHall.getPosition() < initial2){
+    //make joystick value positive, pos is up, neg is down
+    double rightJoy = -OI.atkJoy1.getRawAxis(5);
+    
+    //initials are the physical mins, but code max
+    //max is the code min, but physical max height
+    if (frontRightHall.getPosition() < initial && rearRightHall.getPosition() < initial2 
+    && frontRightHall.getPosition() > max && rearRightHall.getPosition() > max){
+      //in usable range, so go wherever
       frontRight.set(rightJoy);
       rearRight.set(rightJoy);
       System.out.println(rightJoy);
     }
-  else if (frontRightHall.getPosition() >= initial && rearRightHall.getPosition() >= initial2 && rightJoy > 0){
-    frontRight.set(rightJoy);
-    rearRight.set(rightJoy);
-  }
-  else{
-    frontRight.set(0.0);
-    rearRight.set(0.0);
-    System.out.println(rightJoy);
-  }
-   //frontLeft.set(rightJoy);
+    else if ((frontRightHall.getPosition() >= initial || rearRightHall.getPosition() >= initial2) && rightJoy > 0){
+      //at/past physical min(code max) and trying to go up, so go up)
+      frontRight.set(rightJoy);
+      rearRight.set(rightJoy);
+    }
+    else if ((frontRightHall.getPosition() <= max || rearRightHall.getPosition() <= max) && rightJoy < 0){
+      //at/past physical max(code min) and trying to go down, so go down)
+      frontRight.set(rightJoy);
+      rearRight.set(rightJoy);
+    }
+    else{//anything other cases will stop the cascade
+      frontRight.set(0.0);
+      rearRight.set(0.0);
+      System.out.println("no");
+    }
+    //frontLeft.set(rightJoy);
     //rearLeft.set(leftJoy);
     //middleLeft.set(leftJoy);
     //middleRight.set(rightJoy);   
     ///System.out.println(DriveTrain.frontLeftHall.getPosition() + "/t" + DriveTrain.frontRightHall.getPosition() + "/t" + DriveTrain.rearLeftHall.getPosition() + "/t" + DriveTrain.rearRightHall.getPosition());  
   }
-  */
-
+  
+/*
   public static void driveTeleop(){//cargo
     //double leftJoy = OI.atkJoy1.getRawAxis(1);
     double rightJoy = OI.atkJoy1.getRawAxis(5);
@@ -105,6 +117,7 @@ public class DriveTrain extends Subsystem {
     //middleLeft.set(leftSpeed);
     //middleRight.set(rightSpeed);
   }
+  */
 
   public static void infrared(){
   }
